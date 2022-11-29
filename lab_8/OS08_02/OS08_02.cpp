e1ae7d0b1d8ca22f5550560b421aa20db79eed12
Shumskiy Evgeniy <vkzheny@gmail.com>
2022-11-29 21:10:20 +0600

    OS08_02: global rewrite

--- lab_8/OS08_02/OS08_02.cpp
+++ lab_8/OS08_02/OS08_02.cpp
@@ -6,14 +6,26 @@ int main()
 	auto start = std::chrono::system_clock::now();
 	std::chrono::duration<double> elapsed_seconds;
 
+	bool isDisplayed = false;
+	int8_t previous_second = 0;
 	unsigned long i;
 
 
 	for (i = 0; (elapsed_seconds = std::chrono::system_clock::now() - start).count() < 15; i++) {
 
-		if (elapsed_seconds.count() == 5 || elapsed_seconds.count() == 10) {
-			printf("i: %d, s: %d\n", i, int(elapsed_seconds.count()));
+		if (int(elapsed_seconds.count()) != previous_second) {
+			isDisplayed = false;
 		}
+
+		if (!isDisplayed && (
+			int(elapsed_seconds.count()) == 5 || int(elapsed_seconds.count()) == 10
+			)) {
+			isDisplayed = true;
+
+			printf("i: %d, s: %f\n", i, elapsed_seconds.count());
+		}
+
+		previous_second = int(elapsed_seconds.count());
 	}
 
 	printf("final: %d", i);

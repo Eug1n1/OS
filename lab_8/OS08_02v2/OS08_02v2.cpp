dde6b0f24a1a991e65426ecb1d2926319783047b
Shumskiy Evgeniy <vkzheny@gmail.com>
2022-11-29 21:27:48 +0600

    OS08_02v2

---
+++ lab_8/OS08_02v2/OS08_02v2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <chrono>
+
+int main()
+{
+	clock_t current_time, start_time = clock();
+
+	bool isDisplayed = false;
+	int8_t previous_second = 0;
+
+	unsigned long i;
+
+	for (i = 0; (current_time = ((clock() - start_time) / CLOCKS_PER_SEC)) < 15; i++) {
+
+		if (current_time != previous_second) {
+			isDisplayed = false;
+		}
+
+		if (!isDisplayed && (
+			current_time == 5 || current_time == 10
+			)) {
+			isDisplayed = true;
+
+			printf("i: %d, s: %d\n", i, current_time);
+		}
+
+		previous_second = current_time;
+	}
+
+	printf("final: %d", i);
+
+	return EXIT_SUCCESS;
+}

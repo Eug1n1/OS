bec4f1342bd2320d4d6bda2dbdddc6217cc4475c
Eugene <vkzheny@gmail.com>
2022-11-29 10:33:30 +0300

    OS08_05.c: formatting

--- lab_8/linux/OS08_05.c
+++ lab_8/linux/OS08_05.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <time.h>
 
-int main ()
-{
+int main() {
     time_t rawtime;
-    struct tm * timeinfo;
+    struct tm *timeinfo;
 
-    time ( &rawtime );
-    timeinfo = localtime ( &rawtime );
-    printf("%d.%d.%d %d:%d:%d\n", timeinfo->tm_mday, timeinfo->tm_mon, timeinfo->tm_year, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
-  
-  return 0;
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+
+    printf(
+        "%d.%d.%d %d:%d:%d\n", 
+        timeinfo->tm_mday,
+        timeinfo->tm_mon,
+        timeinfo->tm_year,
+        timeinfo->tm_hour,
+        timeinfo->tm_min,
+        timeinfo->tm_sec
+    );
+
+    return 0;
 }
